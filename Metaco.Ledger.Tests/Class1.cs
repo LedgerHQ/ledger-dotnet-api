b7328153dfbf35044c09bea1ffbe9da622dcb34e
Nicolas DORIER <nicolas.dorier@gmail.com>
2015-09-08 17:52:59 +0900

    setup xunit

---
+++ Metaco.Ledger.Tests/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Metaco.Ledger.Tests
+{
+	public class Class1
+	{
+		[Fact]
+		public void Test()
+		{
+		}
+	}
+}
