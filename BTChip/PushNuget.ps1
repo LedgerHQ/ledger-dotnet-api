64816e0388f8f46aa68203960e08a5835072f519
Nicolas DORIER <nicolas.dorier@gmail.com>
2015-10-02 01:07:49 +0900

    fix pushnuget script

--- BTChip/PushNuget.ps1
+++ BTChip/PushNuget.ps1
@@ -4,7 +4,7 @@ C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\BTChip\BTChip.cspr
 nuGet pack
 
 forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE"
-(((dir *.nupkg).Name).Item(0) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
+(((dir *.nupkg).Name) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
 $ver = $Matches.Item(0)
 git tag -a "v$ver" -m "$ver"
 git push --tags
