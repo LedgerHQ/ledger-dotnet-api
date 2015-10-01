53439eae2e8b6dd25e86ff44e54c3b2fd18c4008
Nicolas DORIER <nicolas.dorier@gmail.com>
2015-10-02 01:05:41 +0900

    nuget files

---
+++ BTChip/PushNuget.ps1
@@ -0,0 +1,10 @@
+del *.nupkg
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\BTChip\BTChip.csproj" -p:Configuration=Release
+
+nuGet pack
+
+forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE"
+(((dir *.nupkg).Name).Item(0) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
+$ver = $Matches.Item(0)
+git tag -a "v$ver" -m "$ver"
+git push --tags
