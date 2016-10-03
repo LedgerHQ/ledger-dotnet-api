c56ea247d7e3277a4e24e4f50f8ee00a86f4bca7
NicolasDorier <nicolas.dorier@gmail.com>
2016-10-03 15:06:19 +0900

    update nuget deploy

--- LedgerWallet/PushNuget.ps1
+++ LedgerWallet/PushNuget.ps1
@@ -1,9 +1,9 @@
 del *.nupkg
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\LedgerWallet\LedgerWallet.csproj" -p:Configuration=Release
 
-nuGet pack -Properties Configuration=Release
+nuGet pack LedgerWallet.csproj -Properties Configuration=Release
 
-forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE"
+forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE" -source https://api.nuget.org/v3/index.json"
 (((dir *.nupkg).Name) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
 $ver = $Matches.Item(0)
 git tag -a "v$ver" -m "$ver"
