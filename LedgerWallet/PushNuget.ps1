f86cb1a51955493fa61e9fd7e037b3f1fdb9cbb2
NicolasDorier <nicolas.dorier@gmail.com>
2016-10-03 15:09:45 +0900

    Git ignore nupkg

--- LedgerWallet/PushNuget.ps1
+++ LedgerWallet/PushNuget.ps1
@@ -4,7 +4,7 @@ C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\LedgerWallet\Ledge
 nuGet pack LedgerWallet.csproj -Properties Configuration=Release
 
 forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE -source https://api.nuget.org/v3/index.json"
-(((dir *.nupkg).Name)[0] -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
+(((dir *.nupkg).Name) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
 $ver = $Matches.Item(0)
 git tag -a "v$ver" -m "$ver"
 git push --tags
