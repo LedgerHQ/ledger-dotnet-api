a22a3cbe4eeb3682b24cbd828ce42ab09297fa1f
NicolasDorier <nicolas.dorier@gmail.com>
2017-08-25 22:40:09 +0900

    Update to latest NBitcoin + .net core 2.0 project

--- LedgerWallet/PushNuget.ps1
+++ LedgerWallet/PushNuget.ps1
@@ -1,10 +1,6 @@
-del *.nupkg
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\LedgerWallet\LedgerWallet.csproj" -p:Configuration=Release
-
-nuGet pack LedgerWallet.csproj -Properties Configuration=Release
-
-forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE -source https://api.nuget.org/v3/index.json"
-(((dir *.nupkg).Name) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
-$ver = $Matches.Item(0)
-git tag -a "v$ver" -m "$ver"
-git push --tags
+rm "bin\release\" -Recurse -Force
+dotnet pack --configuration Release
+dotnet nuget push "bin\Release\" --source "https://api.nuget.org/v3/index.json"
+$ver = ((ls .\bin\release\*.nupkg)[0].Name -replace 'NBXplorer\.Client\.(\d+(\.\d+){1,3}).*', '$1')
+# git tag -a "v$ver" -m "$ver"
+# git push --tags
