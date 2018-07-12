e27a5250916e5b493630b271c328c9cf67a76eb1
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-07-12 18:31:01 +1000

    Unit test conversion

--- LedgerWallet.Tests/UnitTest1.cs
+++ LedgerWallet.Tests/UnitTest1.cs
@@ -3,12 +3,12 @@ using Xunit;
 
 namespace LedgerWallet.Tests
 {
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
 
-        }
-    }
+		}
+	}
 }
