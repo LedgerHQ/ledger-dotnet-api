36751bae50266105eb37e8384163a5f04edeed05
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-07-15 08:14:03 +1000

    Formatting

--- LedgerWallet/Transports/HIDDeviceTransportRegistry.cs
+++ LedgerWallet/Transports/HIDDeviceTransportRegistry.cs
@@ -7,16 +7,16 @@ using System.Threading.Tasks;
 
 namespace LedgerWallet.Transports
 {
-    public class IHidDeviceTransportRegistry<T> where T : HIDTransportBase
-    {
-        Func<IHidDevice, T> create;
-        public IHidDeviceTransportRegistry(Func<IHidDevice, T> create)
-        {
-            this.create = create;
-        }
+	public class IHidDeviceTransportRegistry<T> where T : HIDTransportBase
+	{
+		Func<IHidDevice, T> create;
+		public IHidDeviceTransportRegistry(Func<IHidDevice, T> create)
+		{
+			this.create = create;
+		}
 
-#if (!NETSTANDARD2_0)
-        public unsafe IEnumerable<T> GetHIDTransports(IEnumerable<VendorProductIds> ids, params UsageSpecification[] acceptedUsages)
+#if(!NETSTANDARD2_0)
+		public unsafe IEnumerable<T> GetHIDTransports(IEnumerable<VendorProductIds> ids, params UsageSpecification[] acceptedUsages)
 		{
 			return EnumerateIHidDevices(ids, acceptedUsages)
 							.Select(d => GetTransport(d))
@@ -24,20 +24,20 @@ namespace LedgerWallet.Transports
 		}
 #endif
 
-        Dictionary<string, T> _TransportsByDevicePath = new Dictionary<string, T>();
-        private T GetTransport(DeviceInformation device)
-        {
-            lock (_TransportsByDevicePath)
-            {
-                T transport = null;
-                var uniqueId = string.Format("[{0},{1}]", device.VendorId, device.ProductId);
-                if (_TransportsByDevicePath.TryGetValue(uniqueId, out transport))
-                    return transport;
-                var windowsHidDevice = new WindowsHidDevice(device);
-                transport = create(windowsHidDevice);
-                _TransportsByDevicePath.Add(uniqueId, transport);
-                return transport;
-            }
-        }
-    }
+		Dictionary<string, T> _TransportsByDevicePath = new Dictionary<string, T>();
+		private T GetTransport(DeviceInformation device)
+		{
+			lock(_TransportsByDevicePath)
+			{
+				T transport = null;
+				var uniqueId = string.Format("[{0},{1}]", device.VendorId, device.ProductId);
+				if(_TransportsByDevicePath.TryGetValue(uniqueId, out transport))
+					return transport;
+				var windowsHidDevice = new WindowsHidDevice(device);
+				transport = create(windowsHidDevice);
+				_TransportsByDevicePath.Add(uniqueId, transport);
+				return transport;
+			}
+		}
+	}
 }
