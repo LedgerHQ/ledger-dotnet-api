2c10bc024669e341c5e675ae6cd6a1282cbc558a
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-07-21 09:15:17 +1000

    Add an Android provider

---
+++ LedgerWalletAndroidSample/AndroidHIDNetProvider.cs
@@ -0,0 +1,48 @@
+﻿using LedgerWallet.Transports;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Hid.Net;
+using LedgerWallet.HIDProviders;
+
+namespace LedgerWallet.HIDProviders.HIDNet
+{
+    public class AndroidHIDNetProvider : IHIDProvider
+    {
+        public IHIDDevice CreateFromDescription(HIDDeviceInformation hidDevice)
+        {
+            return new AndroidHIDNetDevice(hidDevice);
+        }
+
+        public Task<IEnumerable<HIDDeviceInformation>> EnumerateDeviceDescriptions(IEnumerable<VendorProductIds> vendorProductIds, UsageSpecification [] acceptedUsages)
+        {
+            List<DeviceInformation> devices = new List<DeviceInformation>();
+
+            var collection = WindowsHidDevice.GetConnectedDeviceInformations();
+
+            foreach (var ids in vendorProductIds)
+            {
+                if (ids.ProductId == null)
+                    devices.AddRange(collection.Where(c => c.VendorId == ids.VendorId));
+                else
+                    devices.AddRange(collection.Where(c => c.VendorId == ids.VendorId && c.ProductId == ids.ProductId));
+
+            }
+            var retVal = devices
+                .Where(d =>
+                acceptedUsages == null ||
+                acceptedUsages.Length == 0 ||
+                acceptedUsages.Any(u => d.UsagePage == u.UsagePage && d.Usage == u.Usage)).ToList();
+
+            return Task.FromResult<IEnumerable<HIDDeviceInformation>>(retVal.Select(r => new HIDDeviceInformation()
+            {
+                ProductId = r.ProductId,
+                VendorId = r.VendorId,
+                DevicePath = r.DevicePath,
+                ProviderInformation = r
+            }));
+        }
+    }
+}
